91dca3cca45bc2430d5d59ad440ce2ec8d95b54c
Carlos Ortiz <carlosortiz@MacBook-Pro-de-Carlos.local>
2023-05-15 07:58:57 +0200

    adsfasve

--- srcs/expander.c
+++ srcs/expander.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cortiz <cortiz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: carlosortiz <carlosortiz@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 06:46:40 by cortiz            #+#    #+#             */
-/*   Updated: 2023/05/11 11:43:32 by cortiz           ###   ########.fr       */
+/*   Updated: 2023/05/14 13:29:33 by carlosortiz      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,31 @@ int	is_quote(char c)
 	
 // }
 
+char	*word_after_dollar(int i, char *str)
+{
+	char	*index;
+	int		j;
+
+	j = 0;
+	i++;
+	while (ft_isalpha(str[i + j]))
+		j++;
+	index = malloc(sizeof(char) * (j + 1));
+	index[j] = 0;
+	strncpy(index, &str[i], j);
+	return (index);
+}
+
 void	handle_quotes(char *str)
 {
 	int		i;
-	int		j;
+	// int		j;
+	// int		inquotes;
 	// char	*env_data;
 	char	*index;
 
-	j = 0;
+	// inquotes = 0;
+	// j = 0;
 	i = 0;
 	while (str[i])
 	{
@@ -38,24 +55,21 @@ void	handle_quotes(char *str)
 			i++;
 			while (str[i] != '"')
 			{
-				if (str[i] == '$')
+				if (str[i] == '$' && str[i] != ' ' && str[i] != '\0')
 				{
-					i++;
-					while (ft_isalpha(str[i + j]))
-						j++;
-					index = malloc(sizeof(char) * (j + 1));
-					index[j] = 0;
-					strncpy(index, &str[i], j);
+					// i++;
+					// while (ft_isalpha(str[i + j]))
+					// 	j++;
+					// index = malloc(sizeof(char) * (j + 1));
+					// index[j] = 0;
+					// strncpy(index, &str[i], j);
+					index = word_after_dollar(i, str);
 					printf("%s\n", index);
 					// env_data = get_env_data();
 				}
 				i++;
 			}
 		}
-		// if (str[i] == '\'')
-		// {
-			
-		// }
 		i++;
 	}
 }

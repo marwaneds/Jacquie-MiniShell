ac00345743b7a816868fd05324f7b0c55c245c40
CarlosRTZz <or.carlos.pro@gmail.com>
2023-05-16 11:31:05 +0200

    test

--- srcs/expander.c
+++ srcs/expander.c
@@ -6,7 +6,7 @@
 /*   By: cortiz <cortiz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 06:46:40 by cortiz            #+#    #+#             */
-/*   Updated: 2023/05/15 11:24:08 by cortiz           ###   ########.fr       */
+/*   Updated: 2023/05/16 10:54:19 by cortiz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,31 +33,44 @@ char	*get_env_data(char *index, t_data *data)
 	return (0);
 }
 
-char	*word_after_dollar(int i, char *str)
+char	*word_after_dollar(int i, char *str,int *j)
 {
 	char	*index;
-	int		j;
 
-	j = 0;
 	i++;
-	while (ft_isalpha(str[i + j]))
-		j++;
-	index = malloc(sizeof(char) * (j + 1));
-	index[j] = 0;
-	strncpy(index, &str[i], j);
+	while (ft_isalpha(str[i + *j]))
+		*j += 1;
+	index = malloc(sizeof(char) * (*j + 1));
+	index[*j] = 0;
+	strncpy(index, &str[i], *j);
 	return (index);
+}
+
+char	*expand(int start, int end, char *str, char *new_str)
+{
+	// int	i;
+	char	*expanded;
+
+	(void)new_str;
+	(void)end;
+	expanded = malloc(sizeof(char) * (start + 1));
+	if (!expanded)
+		return (0);//FO FREE ET EXIT;
+	expanded[start] = 0;
+	ft_memcpy(expanded, str, start);
+	return ("salyt");
 }
 
 void	handle_quotes(char *str, t_data *data)
 {
 	int		i;
-	// int		j;
+	int		j;
 	// int		inquotes;
 	char	*env_data;
 	char	*index;
 
 	// inquotes = 0;
-	// j = 0;
+	j = 0;
 	i = 0;
 	while (str[i])
 	{
@@ -66,12 +79,16 @@ void	handle_quotes(char *str, t_data *data)
 			i++;
 			while (str[i] != '"')
 			{
-				if (str[i] == '$' && str[i] != ' ' && str[i] != '\0')
+				if (str[i] == '$' && str[i + 1] != ' ' && str[i + 1] != '\0')
 				{
-					index = word_after_dollar(i, str);
+					index = word_after_dollar(i, str, &j);
 					printf("word after dollar = %s\n", index);
 					env_data = get_env_data(index, data);
 					printf("value of %s = %s\n", index, env_data);
+					printf("valeur de i%d\n", i);
+					printf("valeur de j%d\n", j);
+					expand(i, j, str, env_data);
+					i += j;
 				}
 				i++;
 			}

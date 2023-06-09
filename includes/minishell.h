28f849ff28f630f686b936881c6b4ae8b20fa489
Carlos Ortiz <carlosortiz@MacBook-Pro-de-Carlos.local>
2023-06-09 12:58:56 +0200

    Change include gnl

--- includes/minishell.h
+++ includes/minishell.h
@@ -2,7 +2,7 @@
 # define MINISHELL_H
 
 # include "../libft/libft.h"
-# include "get_next_line.h"
+# include "../srcs/gnl/get_next_line.h"
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>

e3f953743195a29f1359dc1cc779fe06356aeac5
Marwane El Faquiri <mel-faqu@fu-r2-p4.s19.be>
2023-06-12 11:18:43 +0200

    jsp dsl

--- srcs/parser/diff_redirection.c
+++ srcs/parser/diff_redirection.c
@@ -6,11 +6,29 @@
 /*   By: mel-faqu <mel-faqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 10:38:23 by mel-faqu          #+#    #+#             */
-/*   Updated: 2023/06/12 10:55:42 by mel-faqu         ###   ########.fr       */
+/*   Updated: 2023/06/12 11:07:39 by mel-faqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	add_new_redirection(t_lexer *tmp,t_parser *parser)
+{
+	t_lexer *node;
+	int		i1;
+	int		i2;
+	
+	node = lexer_new(ft_strdup(tmp->next->str), tmp->token);
+	if (!node)
+		parser_error(1, parser->data, parser->lexer_list);
+	lexer_adback(&parser->redirections, node);
+	i1 = tmp->i;
+	i2 = tmp->next->i;
+	lexer_delone(&parser->lexer_list, i1);
+	lexer_delone(&parser->lexer_list, i2);
+	parser->num_red++;
+	return (0);
+}
 
 void	rm_redirections(t_parser *parser)
 {
